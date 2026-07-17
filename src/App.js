c1d2aec2b948e02bdc2251e777a03e2bd6feb034
Cindy Hech <archerychinadiva@gmail.com>
2026-07-17 02:04:48 -0400

    firebase and firestore components completed

--- src/App.js
+++ src/App.js
@@ -1,73 +1,138 @@
 import React from "react";
 import "./App.css";
-import { auth } from './firebase/init';
-import { 
+import { auth, db } from "./firebase/init";
+import {
+  collection,
+  addDoc,
+  getDocs,
+  getDoc,
+  doc,
+  query,
+  where,
+  updateDoc,
+  deleteDoc,
+} from "firebase/firestore";
+import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signOut,
-  onAuthStateChanged
+  onAuthStateChanged,
 } from "firebase/auth";
 
 function App() {
   const [user, setUser] = React.useState({});
   const [loading, setLoading] = React.useState(true);
 
+  async function updatePost() {
+    const hardcodedId = "u5bQlC5FZ8Mtb3afAiI3";
+    const postRef = doc(db, "posts", hardcodedId);
+    const post = await getPostById(hardcodedId);
+    console.log(post);
+    const newPost = {
+      ...post,
+      uid: "1",
+      title: "Land a $400K job",
+    };
+    updateDoc(postRef, newPost);
+  }
+
+  function deletePost() {
+    const hardcodedId = "u5bQlC5FZ8Mtb3afAiI3";
+    const postRef = doc(db, "posts", hardcodedId);
+    deleteDoc(postRef);
+  }
+
+  function createPost() {
+    const post = {
+      title: "Finish Interview Section",
+      description: "Do Frontend Simplified",
+      uid: user.uid,
+    };
+    addDoc(collection(db, "posts"), post);
+  }
+
+  async function getAllPosts() {
+    const { docs } = await getDocs(collection(db, "posts"));
+    const posts = docs.map((elem) => ({ ...elem.data(), id: elem.id }));
+    console.log(posts);
+  }
+
+  async function getPostById(id) {
+    const hardcodedId = "u5bQlC5FZ8Mtb3afAiI3";
+    const postRef = doc(db, "posts", id);
+    const postSnap = await getDoc(postRef);
+    return (id = postSnap.data());
+  }
+
+  async function getPostByUid() {
+    const postCollectionRef = await query(
+      collection(db, "posts"),
+      where("uid", "==", user.uid),
+    );
+    const { docs } = await getDocs(postCollectionRef);
+    console.log(docs.map((doc) => doc.data()));
+  }
 
   React.useEffect(() => {
-  const unsubscribe = onAuthStateChanged(auth, (user) => {
-    setLoading(false);
-    console.log(user);
-    if (user) {
-      setUser(user);
-    } else {
-      setUser({}); // Reset to empty object when logged out
-    }
-  });
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setLoading(false);
+      console.log(user);
+      if (user) {
+        setUser(user);
+      } else {
+        setUser({}); // Reset to empty object when logged out
+      }
+    });
 
-  return () => unsubscribe(); // Clean up listener on unmount
-}, []);
-
+    return () => unsubscribe(); // Clean up listener on unmount
+  }, []);
 
   function register() {
-    console.log('register');
-    createUserWithEmailAndPassword(auth,'email@email.com', 'test123')
-    .then((user) => {
-      console.log(user)
-    })
-    .catch((error) => {
-      console.log (error)
-    })
+    console.log("register");
+    createUserWithEmailAndPassword(auth, "email@email.com", "test123")
+      .then((user) => {
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   function login() {
-    signInWithEmailAndPassword(auth, 'email@email.com', 'test123')
-    .then(({user}) => {
-      console.log(user);
-      setUser(user);
-    })
-    .catch((error) => {
-      console.log (error.message);
-    })
+    signInWithEmailAndPassword(auth, "email@email.com", "test123")
+      .then(({ user }) => {
+        console.log(user);
+        setUser(user);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
   }
 
   function logout() {
     signOut(auth)
-    .then(() => {
-      setUser({})  // Clear the user state on logout
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+      .then(() => {
+        setUser({}); // Clear the user state on logout
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
-
-  return <div className="App">
-   <button onClick={register}>Register</button> 
-   <button onClick={login}>Login</button> 
-   <button onClick={logout} >Logout</button>
-   {user.email}
-   {user && user.email ? <span>{user.email}</span> : <span>Loading...</span>}
-    </div>;
+  return (
+    <div className="App">
+      <button onClick={register}>Register</button>
+      <button onClick={login}>Login</button>
+      <button onClick={logout}>Logout</button>
+      {user && user.email ? <span>{user.email}</span> : <span>Loading...</span>}
+      <button onClick={createPost}>Create Post</button>
+      <button onClick={getAllPosts}>Get All Posts</button>
+      <button onClick={getPostById}>Get Post By Id</button>
+      <button onClick={getPostByUid}>Get Post By Uid</button>
+      <button onClick={updatePost}>Update Post</button>
+      <button onClick={deletePost}>Delete Post</button>
+    </div>
+  );
 }
 
 export default App;
